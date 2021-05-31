1580c5576ea1455cd7390088a2231edcd2593619
GoranTadic <71558098+GoranTadic@users.noreply.github.com>
2021-05-31 22:15:17 +0200

    First Testcases

--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -1,52 +1,30 @@
 package at.ac.fhcampuswien;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.stream.*;
 
 public class AppTest {
 
-    public static void main(String[] args) {
-        String password = "Hello5";
-
-
-
+    @Test
+    @DisplayName("Has password valid length - to short")
+    public void passwordLength1(){
+        App pw = new App();
+        assertFalse(pw.passwordLength("Hello"));
     }
 
     @Test
-    public boolean passwordLength(String password){
-        return password.length() > 7 && password.length() < 26;
+    @DisplayName("Has password valid length - to long")
+    public void passwordLength2(){
+        App pw = new App();
+        assertFalse(pw.passwordLength("HelloWorldThisPasswordIsTooLong"));
     }
 
     @Test
-    public boolean lowerAndUpperCase(String password){
-        char[] chars = password.toCharArray();
-        boolean upper = false;
-        boolean lower = false;
-
-        for(char a : chars){
-            if(a > 96 && a < 123) lower = true;
-            if(a > 64 && a < 91) upper = true;
-        }
-        return upper && lower;
-    }
-
-    @Test
-    public boolean containsDigits(String password){
-        char[] chars = password.toCharArray();
-
-
-        for(char a : chars){
-            if(Character.isDigit(a)) return true;
-        }
-        return false;
-    }
-
-    @Test
-    public boolean containsSpecialSign(String password){
-        boolean sign = false;
-
-        Character.
-        return sign;
+    @DisplayName("Has password valid length - true")
+    public void passwordLength3(){
+        App pw = new App();
+        assertTrue(pw.passwordLength("HelloWorld"));
     }
 }
